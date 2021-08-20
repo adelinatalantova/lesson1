daacd1f0136386ab1c2e70985c3d6101f2f8e35c
Admin <adelatalantova23@gmail.com>
2021-08-20 20:33:07 +0300

    gfh

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Аделя";
+        String firstname = "Мурадилова";
+        String age = " 15 лет";
+        System.out.println("Меня зовут " + name);
+        System.out.println("Моя фамилия " + firstname );
+        System.out.println("Мне " + age );
+    }
+}
